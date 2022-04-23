436ca961e2b6f46772f0294756e7838b4ffe5fca
RodrigoTadeu <rodrigotadeu61@gmail.com>
2022-04-23 19:52:39 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import './estilo.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  const {register, handleSubmit, setValue, setFocus} = useForm()
+
+  const onSubmit = (e) => {
+      console.log(e)
+  }
+   
+   const onblurCep = (ev) => {
+      const cep = ev.target.value.replace(/\D/g, '')
+      console.log(cep)
+
+      if (cep.length !== 8 ) {
+        return
+      }
+   
+      fetch (`https://viacep.com.br/ws/${cep}/json/`)
+          .then ((res) => res.json())
+          .then ((data) => {
+            setValue('logradouro', data.logradouro)
+            setValue('bairro', data.bairro)
+            setValue('cidade', data.localidade)
+            setValue('uf', data.uf)
+            setFocus('numero')
+           
+          })
+          
+        }
+   
+    return (
+    <div className='tudo'>
+      <h1 className='t'>
+        Consulta endereço ViaCep
+      </h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+  
+        <div>
+          <label> CEP: </label> 
+          <input type='text' {...register('cep')} onBlur={onblurCep}></input>
+        </div>
+        <div>
+        <label> Logradouro: </label> 
+        <input type='text' {...register('logradouro')}></input>
+        </div>
+        <div>
+        <label> Número: </label> 
+        <input type='text' {...register('numero')}></input>
+        </div>
+        <div>
+        <label> Bairro: </label> 
+        <input type='text' {...register('bairro')}></input>
+        </div>
+        <div>
+        <label> Cidade: </label> 
+        <input type='text' {...register('cidade')}></input>
+        </div>
+        <div>
+        <label> Estado: </label> 
+        <input type='text' {...register('uf')}></input>
+        </div>
+
+        <button type='submit' className='b'>Enviar</button>
+      </form>
     </div>
-  );
+  )
 }
-
-export default App;
